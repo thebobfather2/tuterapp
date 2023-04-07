1f5a3b10522420902de8aa5db77cc82c24b18c95
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-04-06 22:32:11 -0400

    added second card

--- client/src/pages/tuters.js
+++ client/src/pages/tuters.js
@@ -4,13 +4,20 @@ import Card from "../components/Card";
 const Tuters = () => {
     return (
         <div>
-          <Card
-            imageUrl="../img/tuter1.jpg"
-            imageAlt="Bryan"
-            title="Bryan"
-            description="Bryan specializes in web3 development and has experience with react, CSS, html, and javascript."
-            buttonText="Learn more"
-          />
+            <Card
+                imageUrl="../img/tuter1.jpg"
+                imageAlt="Bryan"
+                title="Bryan"
+                description="Bryan specializes in web3 development and has experience with react, CSS, html, and javascript."
+                buttonText="Learn more"
+            />
+            <Card
+                imageUrl="../img/tuter1.jpg"
+                imageAlt="Sarah"
+                title="Sarah"
+                description="Sarah has experience with react, CSS, html, and javascript."
+                buttonText="Learn more"
+            />
         </div>
       );
     };
