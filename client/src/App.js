ea1032d22680aaf2ad99703c8fa0db761cc87e00
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-07 11:08:56 -0400

    changed casing on tuters in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
-import Tuters from './pages/Tuters';
+import Tuters from './pages/tuters';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Home');
