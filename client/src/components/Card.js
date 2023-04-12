b529ce1f934c29b37ac0debbfaac936bf8d64f16
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-12 19:17:00 -0400

    style added to cads

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="card">
-      <img src={props.imageUrl} alt={props.imageAlt} />
+      <img src={props.imageUrl} alt={props.imageAlt} style={{marginTop: "25px", borderRadius: "20px", border: "solid", borderColor: "#ABDCD6", display: "block", margin: "0 auto", borderWidth: "10px"}} />
       <div className="card-content">
         <h2>{props.title}</h2>
         <p>{props.description}</p>
