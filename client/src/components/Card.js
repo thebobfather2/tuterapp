3c9b2cb8abb4373754cbe68f28941699910c9f19
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-14 15:07:56 -0400

    made cards in tuter bigger

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="card" style={{padding: "50px"}}>
-      <img src={props.imageUrl} alt={props.imageAlt} style={{borderRadius: "20px", border: "solid", borderColor: "#ABDCD6", display: "block", margin: "0 auto", borderWidth: "10px", maxWidth: "30%"}} />
+      <img src={props.imageUrl} alt={props.imageAlt} style={{borderRadius: "20px", border: "solid", borderColor: "#ABDCD6", display: "block", margin: "0 auto", borderWidth: "10px", maxWidth: "60%"}} />
       <div className="card-content">
         <h2 style={{paddingTop: "20px", textAlign: "center", display: "block", margin: "0 auto"}}>{props.title}</h2>
         <p style={{padding: "20px", textAlign: "center", display: "block", margin: "0 auto"}}>{props.description}</p>
