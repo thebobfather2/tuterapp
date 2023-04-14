fb18273bf62b30f8fbbc9eb55fda82d76cf0eb26
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-14 14:53:35 -0400

    added mongo atlas

--- client/src/components/HomeworkCard.js
+++ client/src/components/HomeworkCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const data = db.tuters.find({});
-console.log(data);
+// const data = db.tuters.find({});
+// console.log(data);
 
 const HomeworkCard = (props) => {
   return (
     <div className="card">
       <img src={props.imageUrl} alt={props.imageAlt} />
       <div className="card-content">
-        <h2>{data}</h2>
+        {/* <h2>{data}</h2> */}
       </div>
     </div>
   );
